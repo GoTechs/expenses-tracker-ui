cb0ae28f7b9b76257161f208477b80ce744ef324
slimane.kadour <slimane.kadour@cerence.com>
2020-05-12 00:12:14 -0400

    add expense modal container; add expense selector;

---
+++ src/containers/expensesModalContainer/index.js
@@ -0,0 +1,163 @@
+import React, { Component } from "react";
+import _ from "lodash";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import AddModalContainer from "../../components/Modal";
+import { connect } from "react-redux";
+import actions from "../../store/actions";
+import "./styles.css";
+
+class AddExpensesModal extends Component {
+  state = {
+    formConfig: {
+      amount: {
+        elementType: "input",
+        label: "Amount*",
+        elementConfig: {
+          type: "text",
+          placeholder: "",
+        },
+        validation: {
+          required: true,
+          isNumeric: true,
+        },
+        valid: false,
+        touched: false,
+        errorText: "A number is expected.",
+        value: "",
+      },
+      description: {
+        elementType: "input",
+        label: "Description*",
+        elementConfig: {
+          type: "text",
+          placeholder: "",
+        },
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+        errorText: "Description is required",
+        value: "",
+      },
+    },
+    isValidForm: false,
+    loading: false,
+  };
+ 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.selectedExpense !== prevState.selectedExpense) {
+      const formData = { ...prevState.formConfig };
+      console.log(nextProps.selectedExpense)
+      formData["amount"].value = _.get(nextProps, "selectedExpense.amount");
+      formData["description"].value = _.get(nextProps, "selectedExpense.description");;
+      return { formConfig: formData };
+    }
+  }
+
+  inputChangedHandler = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    let formIsValid = true;
+    console.log(value, name);
+    const updateFormConfig = { ...this.state.formConfig };
+    updateFormConfig[name].value = value;
+    updateFormConfig[name].touched = true;
+    const rules = _.get(updateFormConfig[name], "validation");
+    updateFormConfig[name].valid = this.checkValidity(value, rules);
+    for (let identifier in updateFormConfig) {
+      formIsValid = updateFormConfig[identifier].valid && formIsValid;
+    }
+    this.setState({ formConfig: updateFormConfig, isValidForm: formIsValid });
+  };
+
+  checkValidity = (value, rules) => {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+    return isValid;
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {};
+    const { isValidForm } = this.state;
+    const formConfigData = { ...this.state.formConfig };
+    if (isValidForm) {
+      Object.keys(formConfigData).forEach(
+        (identifier) =>
+          (formData[identifier] = formConfigData[identifier].value)
+      );
+      this.props.showEditExpense
+        ? this.props.editExpense(formData)
+        : this.props.addExpense(formData);
+      this.props.closeModal();
+    }
+  };
+
+  render() {
+    const { formConfig } = this.state;
+    const { showAddExpense, showEditExpense, selectedExpense } = this.props;
+    const show = showEditExpense ? showEditExpense : showAddExpense;
+    return (
+      <div className="add-expense-modal">
+        <AddModalContainer
+          show={show}
+          onHide={this.props.closeModal}
+          title={showEditExpense ? " Edit expense" : "Add new expense"}
+        >
+          <form onSubmit={this.handleSubmit} autoComplete="false">
+            {_.map(formConfig, (input, key) => (
+              <Input
+                key={key}
+                name={key}
+                elementType={_.get(input, "elementType")}
+                label={_.get(input, "label")}
+                elementConfig={_.get(input, "elementConfig")}
+                errorText={_.get(input, "errorText")}
+                value={_.get(input, "value")}
+                valid={_.get(input, "valid")}
+                shouldValidate={_.get(input, "validation")}
+                touched={_.get(input, "touched")}
+                changed={this.inputChangedHandler}
+              />
+            ))}
+            <div className="btns-group">
+              <Button cancel onClick={this.props.closeModal}>
+                Cancel
+              </Button>
+              <Button primary type="submit" disabled={!this.state.isValidForm}>
+                Confirm
+              </Button>
+            </div>
+          </form>
+        </AddModalContainer>
+      </div>
+    );
+  }
+}
+
+AddExpensesModal.propTypes = {};
+const mapStateToProps = (state) => {
+  return {
+    expenses: _.get(state, "expensesReducer.expenses"),
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    editExpense: (id) => dispatch(actions.expensesActions.editExpense(id)),
+    addExpense: (datForm) =>
+      dispatch(actions.expensesActions.addExpense(datForm)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddExpensesModal);
---
+++ src/store/selectors/expensesSelector.js
@@ -0,0 +1,23 @@
+import { createSelector } from "reselect";
+import _ from "lodash";
+
+const getExpensesData = (state) => _.get(state, "expensesReducer.expenses");
+
+export const getSumExpenses = createSelector([getExpensesData], (expenses) => {
+  return expenses && sum(expenses);
+});
+
+export const getTotalTax = createSelector([getExpensesData], (expenses) => {
+  const sumTotalAmount = sum(expenses);
+  return expenses && (sumTotalAmount + (sumTotalAmount * 15) / 100).toFixed(2);
+});
+
+export const selectExpenseById = createSelector(
+  [getExpensesData],
+  (expenses) => (id) => {
+    return expenses.find((item) => item._id === id) ;
+  }
+);
+
+const sum = (expenses) =>
+  expenses && expenses.reduce((accum, current) => (accum += current.amount), 0);
